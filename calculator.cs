15be0d49c665655009171a46cea825a3441c2150
Felipe Fernandes <117548759+felovers@users.noreply.github.com>
2022-11-30 20:36:38 -0300

    Update calculator.cs

--- calculator.cs
+++ calculator.cs
@@ -1,60 +1,60 @@
 
 Console.WriteLine("------ MENU ------");
-Console.WriteLine("1 - Soma");
-Console.WriteLine("2 - Subtração");
-Console.WriteLine("3 - Multiplicação");
-Console.WriteLine("4 - Divisão");
+Console.WriteLine("1 - Sum");
+Console.WriteLine("2 - Subtraction");
+Console.WriteLine("3 - Multiplication");
+Console.WriteLine("4 - Division");
 Console.WriteLine("------------------");
 
-Console.WriteLine("Digite o número da opção desejada:");
-int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number that represents your desired option:");
+int option = Convert.ToInt32(Console.ReadLine());
 
-switch (x)
+switch (option)
 {
     case 1:
-        Console.WriteLine("------ SOMA ------");
-        Console.WriteLine("Digite o primeiro número:");
+        Console.WriteLine("------ SUM ------");
+        Console.WriteLine("Enter the first number:");
         int n1 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Digite o segundo número:");
+        Console.WriteLine("Enter the second number:");
         int n2 = Convert.ToInt32(Console.ReadLine());
-        int soma = n1 + n2;
-        Console.WriteLine($"O resultado da soma é {soma}.");
+        int sum = n1 + n2;
+        Console.WriteLine($"The result of the sum is: {sum}.");
         break;
     case 2:
-        Console.WriteLine("------- SUBTRAÇÃO ------");
-        Console.WriteLine("Digite o primeiro número:");
+        Console.WriteLine("------- SUBTRACTION ------");
+        Console.WriteLine("Enter the first number:");
         int n3 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Digite o segundo número:");
+        Console.WriteLine("Enter the second number:");
         int n4 = Convert.ToInt32(Console.ReadLine());
-        int subtração = n3 - n4;
-        Console.WriteLine($"O resultado da subtração é {subtração}."); ;
+        int subtraction = n3 - n4;
+        Console.WriteLine($"The result of the subtraction is: {subtraction}."); ;
         break;
     case 3:
-        Console.WriteLine("------ MULTIPLICAÇÃO ------");
-        Console.WriteLine("Digite o primeiro número:");
+        Console.WriteLine("------ MULTIPLICATION ------");
+        Console.WriteLine("Enter the first number:");
         int n5 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Digite o segundo número:");
+        Console.WriteLine("Enter the second number:");
         int n6 = Convert.ToInt32(Console.ReadLine());
-        int multiplicação = n5 * n6;
-        Console.WriteLine($"O resultado da multiplicação é {multiplicação}.");
+        int multiplication = n5 * n6;
+        Console.WriteLine($"The result of the multiplication is: {multiplication}.");
         break;
     case 4:
-        Console.WriteLine("------ DIVISÃO ------");
-        Console.WriteLine("Digite o primeiro número:");
+        Console.WriteLine("------ DIVISION ------");
+        Console.WriteLine("Enter the first number:");
         double n7 = Convert.ToInt32(Console.ReadLine());
         while (true)
         {
-            Console.WriteLine("Digite o segundo número:");
+            Console.WriteLine("Enter the second number:");
             double n8 = Convert.ToInt32(Console.ReadLine());
             if (n8 != 0)
             {
-                double divisão = n7 / n8;
-                Console.WriteLine($"O resultado da divisão é {divisão}.");
+                double division = n7 / n8;
+                Console.WriteLine($"The result of the division is: {division}.");
                 break;
             }
             else
             {
-                Console.WriteLine("Digite um número maior que 0.");
+                Console.WriteLine("Enter a number greater than 0.");
             }
         }
         break;
